c2c460c07e8852c7b9715a93ae9425bdc7279210
kwannieng <78864242+kwannieng@users.noreply.github.com>
2023-03-03 23:04:50 +0000

    completed lib

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
